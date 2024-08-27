ffbcc7b187d01547dbeec811985d1b602429eaee
Patolord <rodrigo_luizp@hotmail.com>
2024-08-27 20:28:47 -0300

    refactor: Remove unused sections from Home page
    This commit removes the unused sections from the Home page, including the "See It in Action", "FAQ", and "Warehouse Watcher Benefits" sections. These sections were not being displayed or utilized, and removing them improves the overall clarity and focus of the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,34 +17,6 @@ export default function Home() {
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
 
-      <SectionTitle
-        preTitle="See It in Action"
-        title="Discover the Power of Real-Time Inventory Management"
-      >
-        Watch how Warehouse Watcher transforms your inventory process. With its
-        intuitive interface and real-time updates, youll wonder how you ever
-        managed without it. See why our users are calling it a game-changer in
-        warehouse management.
-      </SectionTitle>
-
-
-      <SectionTitle preTitle="FAQ" title="Common Questions About Warehouse Watcher">
-        Get answers to frequently asked questions about Warehouse Watcher.
-        Learn more about its features, how it works without integration, and
-        how it can be customized for your specific needs.
-      </SectionTitle>
-
-      <SectionTitle
-        preTitle="Warehouse Watcher Benefits"
-        title="Why Warehouse Watcher is Your Best Inventory Solution"
-      >
-        Warehouse Watcher is a cutting-edge, real-time warehouse and inventory system.
-        Built with a clean UI and user-friendly design, it caters to both businesses
-        and customers. As an anti-ERP solution, it requires no integration with other
-        software, making it a standalone powerhouse for inventory management.
-      </SectionTitle>
-
-      <Faq />
 
     </Container>
   );
